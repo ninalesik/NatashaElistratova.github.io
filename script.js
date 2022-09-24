fe330b22536f0126b45e92b8f4bf0079567eb069
Oksanaka848 <okmitka@mail.ru>
2022-09-24 17:30:22 +0300

    add view more button

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ function myFunction(imgs) {
       moreText.style.display = "inline";
     }
   }
+
+  function myFunctiontextabout() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("viewmore");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+        } 
+    
+    else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+
+  }
